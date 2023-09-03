a04406c2efffab25cd1cf2bccd721654bcae7401
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-09-03 20:51:44 -0300

    terminando a carcaça da página de perfil

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { api } from '@/lib/api'
 import { getUser } from '@/lib/auth'
 import { UserIDJwtPayload } from 'jsonwebtoken'
+import dayjs from 'dayjs'
 
 import { cookies } from 'next/headers'
+
 interface User {
   id: number
   email: string
@@ -17,7 +19,14 @@ interface User {
     friendRequest: { id: number }
   }
 }
-
+interface Posts {
+  id: number
+  userId: number
+  postTitle: string
+  postDescription: string
+  isPublic: true
+  postDate: Date
+}
 export default async function profile({ params }: { params: { id: string } }) {
   let token: null | UserIDJwtPayload = null
   try {
@@ -31,8 +40,14 @@ export default async function profile({ params }: { params: { id: string } }) {
 
   if (isNaN(+params.id)) return <h1>Id Must be a number</h1>
   const stringToken = cookies().get('token')?.value
-  const response = await api.get(`/user/${params.id}`)
-  const user: User = response.data.user
+  const responseU = await api.get(`/user/${params.id}`)
+  const responseP = await api.get(`/posts/user/${params.id}`, {
+    headers: {
+      Authorization: stringToken,
+    },
+  })
+  const posts: Posts[] = responseP.data
+  const user: User = responseU.data.user
   console.log(user)
 
   return (
@@ -45,6 +60,19 @@ export default async function profile({ params }: { params: { id: string } }) {
       </div>
       <article className="w-full">
         <h1>Time Line</h1>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              {/* aqui virá o perfil antes da postagem acho que vou componentizar isso já que devo usar para postagens externas também */}
+              {/* Tenho que colocar uma rota para update de imagens nos posts */}
+              <h3>{post.postTitle}</h3>
+              {/* Colocar dayjs aqui */}
+              <span>{dayjs(post.postDate).format('MMMM D, YYYY h:mm A')}</span>
+              <p>{post.postDescription}</p>
+              {/* aqui virá a imagem */}
+            </div>
+          )
+        })}
       </article>
     </main>
   )
